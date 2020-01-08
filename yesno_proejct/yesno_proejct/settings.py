1fa45236f3accb9aee30ed21343945090260fbc3
kaotu <kao-tu@hotmail.co.th>
2020-01-08 14:48:54 +0700

    Add path dir

--- yesno_proejct/yesno_proejct/settings.py
+++ yesno_proejct/yesno_proejct/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'yesno_proejct.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR + '/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
