4d836e3cf1d6c43fb5e5a292689abf71d5f27374
kaotu <kao-tu@hotmail.co.th>
2020-01-08 14:49:48 +0700

    Add path :new_moon_with_face:

--- yesno_proejct/yesno_proejct/urls.py
+++ yesno_proejct/yesno_proejct/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from answers.views import answer_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('answer/', answer_views, name='answer_views'),
 ]
