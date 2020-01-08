94d8a7d10a49a4f0cf7d7c1805a7de59e5defe64
kaotu <kao-tu@hotmail.co.th>
2020-01-08 16:23:29 +0700

    Change fn to class base :new_moon_with_face:

--- yesno_proejct/answers/views.py
+++ yesno_proejct/answers/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views import View
+
+from answers.models import Answer
+
+def answer_views(requests):
+    answers = Answer.objects.all()
+    answer = random.choice(answers)
+    return render(requests, 'answer.html', context={'answer': answer})
+
+def create_answer_views(requests):
+    return render(requests, 'create_answer.html')
+
+def add_create_answer_views(requests):
+    result_text = requests.POST['text']
+    result_image = requests.POST['image']
+    create_answer = Answer.objects.create(text=result_text, image=result_image)
+    return redirect('answer_views')
+
+class CreateAnswerView(View):
+    def get(self, requests, *args, **kwargs):
+        return render(requests, 'create_answer.html')
+
+    def post(self, requests, *args, **kwargs):
+        result_text = requests.POST['text']
+        result_image = requests.POST['image']
+        create_answer = Answer.objects.create(text=result_text, image=result_image)
+        return redirect('answer_views')
+        
